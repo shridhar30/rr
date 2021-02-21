bd86febc201b336e24bc36c62a8d1fb2d29e08de
shridhar30 <78227350+shridhar30@users.noreply.github.com>
2021-02-21 01:41:59 -0800

    Update settings.py

--- rrcatering/settings.py
+++ rrcatering/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#3oonrc-7g7@_#ep+@@tj+75^pl*q+u92shoc74dnesocj)&8f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000']
+ALLOWED_HOSTS = ['127.0.0.1:8000' 'rrcatering.herokuapp.com']
 
 STATICFILES_DIRS = [
     "/projects/1st/rrcaterings/static",
