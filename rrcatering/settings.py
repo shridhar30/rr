ba4662d4d1313494d8a843a6376bcef55e515f01
shridhar30 <78227350+shridhar30@users.noreply.github.com>
2021-02-26 04:10:10 -0800

    Update settings.py

--- rrcatering/settings.py
+++ rrcatering/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,7 +131,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 WHITENOISE_USE_FINDERS = True
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
